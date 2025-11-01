ed9fc9ad31e726d87c6d4e04a41c46b4f8e89d6e
kusdmi <kusdmi@yandex.ru>
2025-11-01 16:51:44 +0300

    home_work_5

--- home_work_5.py
+++ home_work_5.py
@@ -1,2 +1,49 @@
-#home_work_5
-print('hello')
+import threading
+import time
+#Задание 1
+# def square():
+#     for i in range(1, 11):
+#         print(f'Квадрат {i} = {i**2}')
+#
+# def cube():
+#     for i in range(1, 11):
+#         print(f'Куб {i} = {i**3}')
+#
+# if __name__ == '__main__':
+#     t1 = threading.Thread(target=square)
+#     t2 = threading.Thread(target=cube)
+#
+# t1.start()
+# t2.start()
+#
+# t1.join()
+# t2.join()
+#
+# print("Вычисления завершены!")
+
+
+#Задание 2
+
+# def square():
+#     for i in range(1, 11):
+#         print(f'Квадрат {i} = {i**2}')
+#         time.sleep(1)
+#
+# def cube():
+#     for i in range(1, 11):
+#         print(f'Куб {i} = {i**3}')
+#         time.sleep(1)
+#
+# if __name__ == '__main__':
+#     t1 = threading.Thread(target=square)
+#     t2 = threading.Thread(target=cube)
+#
+# t1.start()
+# t2.start()
+#
+# t1.join()
+# t2.join()
+#
+# print("Вычисления завершены!")
+
+
